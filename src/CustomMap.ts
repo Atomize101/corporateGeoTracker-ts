d5107e834bbf65d3135929da51d8aebf1c80379e
Chris Patrick <atomize101@gmail.com>
2021-01-22 17:08:51 -0500

    Creating the infoWindow pop up when hovering over a marker on map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,13 +1,10 @@
-import { User } from './User';
-import { Company } from './Company';
-
 // Instructions to every other class
 // on how they can be an argument to 'addMarker'
 interface Mappable {
-    location: {
-        lat: number;
-        lng: number;
-    }
+	location: {
+		lat: number;
+		lng: number;
+	};
 }
 
 export class CustomMap {
@@ -24,25 +21,19 @@ export class CustomMap {
 	}
 
 	addMarker(mappable: Mappable): void {
-        new google.maps.Marker({
+		const marker = new google.maps.Marker({
 			map: this.googleMap,
 			position: {
 				lat: mappable.location.lat,
 				lng: mappable.location.lng,
 			},
 		});
-	}
+		marker.addListener('click', () => {
+			const infoWindow = new google.maps.InfoWindow({
+				content: 'Hi There',
+			});
 
-/* 
-
-	addCompanyMarker(company: Company): void {
-		new google.maps.Marker({
-			map: this.googleMap,
-			position: {
-				lat: company.location.lat,
-				lng: company.location.lng,
-			},
+			infoWindow.open(this.googleMap, marker);
 		});
 	}
 }
-*/
